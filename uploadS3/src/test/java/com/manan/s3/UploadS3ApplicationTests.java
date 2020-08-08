b01c3dd37b785abb6e67caef016b64f397644cd3
unknown <cognizantjava08@iiht.tech>
2020-08-08 13:19:20 +0530

    Upload S3 project

---
+++ uploadS3/src/test/java/com/manan/s3/UploadS3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manan.s3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UploadS3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
